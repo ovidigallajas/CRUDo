8254671e66dca9c2c9dafb2fb201d5f59a727b32
ovidigallajas <77326408+ovidigallajas@users.noreply.github.com>
2021-02-24 18:36:25 +0100

    Add files via upload

---
+++ Mockups/cookie.js
@@ -0,0 +1,16 @@
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "black"
+    },
+    "button": {
+      "background": "#ffffff"
+    }
+  },
+  "showLink": false,
+  "content": {
+	"message": "Necesitamos usar cookies para que funcione todo, si permanece aquí acepta su uso",
+    "dismiss": "¡Aceptar!"
+  }
+});});
